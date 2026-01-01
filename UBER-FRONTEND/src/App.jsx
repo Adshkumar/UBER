de664a544d67f4c432fc19140ac4ca47c214a840
adarsh Kumar <adarsh99733207@gmail.com>
2026-01-01 19:00:07 +0530

    Fix Home import and CaptainLogin component case in App.jsx

--- UBER-FRONTEND/src/App.jsx
+++ UBER-FRONTEND/src/App.jsx
@@ -5,7 +5,7 @@ import UserLogin from './pages/UserLogin'
 import UserSignup from './pages/UserSignup'
 import CaptainLogin from './pages/CaptainLogin'
 import CaptainSignup from './pages/CaptainSignup'
-import Home from './pages/Home'
+import Home from './pages/home'
 import UserProtectWrapper from './pages/UserProtectWrapper'
 import UserLogout from './pages/UserLogout'
 import CaptainHome from './pages/CaptainHome'
@@ -26,7 +26,7 @@ const App = () => {
         <Route path='/captain-riding' element={<CaptainRiding />} />
 
         <Route path='/signup' element={<UserSignup />} />
-        <Route path='/captain-login' element={<Captainlogin />} />
+        <Route path='/captain-login' element={<CaptainLogin />} />
         <Route path='/captain-signup' element={<CaptainSignup />} />
         <Route path='/home'
           element={
